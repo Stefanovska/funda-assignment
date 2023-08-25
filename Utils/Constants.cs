00e61960a8c0a1ab4d9d3f116e4912af77acb79b
Dijana <dijana.stefanovska@ymail.com>
2023-08-25 20:29:22 +0200

    fix: moved periodic service hours in constants

--- Utils/Constants.cs
+++ Utils/Constants.cs
@@ -4,6 +4,8 @@
 	{
         public static readonly string AGENTS_MEMORY_CACHE_KEY = "AGENTS";
         public static readonly string AGENTS_WITH_GARDEN_MEMORY_CACHE_KEY = "AGENTS_WITH_GARDEN";
+
+        public static readonly int PERIODIC_SERVICE_HOURS_INTERVAL = 1;
     }
 }
 
