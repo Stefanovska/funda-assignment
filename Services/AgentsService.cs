975807f004a59f2053a3f4e7ef89fcac9f6ae202
Dijana <dijana.stefanovska@ymail.com>
2023-08-25 20:17:03 +0200

    feat: moved IAgents service interface in separate file

--- Services/AgentsService.cs
+++ Services/AgentsService.cs
@@ -114,12 +114,3 @@ namespace funda_assignment.Services
         }
     }
 }
-
-public interface IAgentsService
-{
-    Task<List<Agent>> FetchAgents(string requestUri);
-    List<Agent> ListAgents();
-    List<Agent> ListAgentsWithGarden();
-    Task SaveAgentsWithMostProperties();
-    Task SaveAgentsWihMostPropertiesWithGarden();
-}
---
+++ Services/IAgentsService.cs
@@ -0,0 +1,14 @@
+﻿using funda_assignment.Models;
+
+namespace funda_assignment.Services
+{
+    public interface IAgentsService
+    {
+        Task<List<Agent>> FetchAgents(string requestUri);
+        List<Agent> ListAgents();
+        List<Agent> ListAgentsWithGarden();
+        Task SaveAgentsWithMostProperties();
+        Task SaveAgentsWihMostPropertiesWithGarden();
+    }
+}
+
